9a4331cc9950744677e9c39845ff61449b8fef56
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-05-22 01:30:53 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.nixxcode.jvmbrotli.common.BrotliLoader;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(BrotliLoader.isBrotliAvailable());
+    }
+}
