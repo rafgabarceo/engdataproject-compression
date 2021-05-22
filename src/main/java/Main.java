70b397eb34bc5364b854c51f5b7df8d33c8887b1
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-05-22 14:34:08 +0800

    Tested compress / decompress with LZMA.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,16 @@
-import lombok.AllArgsConstructor;
-import utils.LoadUtil;
+import utils.*;
 
 import java.io.File;
 import java.util.List;
 
 public class Main {
     private static final LoadUtil loadUtil = new LoadUtil();
+    private static final CompressUtil compressUtil = new CompressUtil();
 
     public static void main(String[] args) throws Exception {
+        compressUtil.initializeCompression(); // should be run at the start of the application
         List<File> loadedFilesForCompression = loadUtil.loadFiles(args);
-        loadedFilesForCompression.forEach((file) -> {
-            System.out.println(file.getAbsolutePath());
-        });
+        compressUtil.lzmaCompress(loadedFilesForCompression.get(0));
+        compressUtil.lzmaDecompress(new File("./compressed.lzma"));
     }
 }
---
+++ src/main/java/utils/CompressUtil.java
@@ -0,0 +1,64 @@
+package utils;
+
+import com.nixxcode.jvmbrotli.common.BrotliLoader;
+import org.apache.commons.compress.compressors.lzma.LZMACompressorInputStream;
+import org.apache.commons.compress.compressors.lzma.LZMACompressorOutputStream;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CompressUtil {
+
+
+    public void initializeCompression(){
+        BrotliLoader.isBrotliAvailable();
+    }
+
+    public void lzmaCompress(File file) throws Exception {
+        try{
+            InputStream in = Files.newInputStream(Path.of(file.getPath()));
+            OutputStream fout = Files.newOutputStream(Path.of("compressed.lzma"));
+            BufferedOutputStream out = new BufferedOutputStream(fout);
+            LZMACompressorOutputStream lzOut = new LZMACompressorOutputStream(out);
+            final byte[] buffer = new byte[1024];
+            int n = 0;
+            while(-1 != (n = in.read(buffer))){
+                lzOut.write(buffer, 0, n);
+            }
+            lzOut.close();
+            in.close();
+        } catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public void lzmaDecompress(File file) throws Exception {
+        try {
+            InputStream fin = Files.newInputStream(Path.of(file.getPath()));
+            BufferedInputStream in = new BufferedInputStream(fin);
+            OutputStream out = Files.newOutputStream(Paths.get("decompressedFile.txt"));
+            LZMACompressorInputStream lzmaIn = new LZMACompressorInputStream(in);
+            final byte[] buffer = new byte[1024];
+            int n = 0;
+            while (-1 != (n = lzmaIn.read(buffer))){
+                out.write(buffer, 0, n);
+            }
+            out.close();
+            lzmaIn.close();
+        } catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public boolean brotliCompress(File file){
+        return false;
+    }
+
+    public boolean brotliDecompress(File file){
+        return false;
+    }
+
+
+}
