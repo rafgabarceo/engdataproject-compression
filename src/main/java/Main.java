d3ec3d0125143b888058bef989ef160da8a00d24
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-05-22 15:46:10 +0800

    Compress / decompress with logs for LZMA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import utils.*;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -10,7 +11,33 @@ public class Main {
     public static void main(String[] args) throws Exception {
         compressUtil.initializeCompression(); // should be run at the start of the application
         List<File> loadedFilesForCompression = loadUtil.loadFiles(args);
-        compressUtil.lzmaCompress(loadedFilesForCompression.get(0));
-        compressUtil.lzmaDecompress(new File("./compressed.lzma"));
+        System.out.println("Compressing files...");
+
+        // compression
+        Arrays.stream(args).forEach((file) -> {
+            try {
+                long startTime = System.currentTimeMillis();
+                compressUtil.lzmaCompress(loadUtil.loadFile(file).getAbsoluteFile());
+                long endTime = System.currentTimeMillis();
+                System.out.println("Compression time of: " + file + " is " + (endTime - startTime) + "ms");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        loadedFilesForCompression.forEach((file) -> {
+
+            try {
+                String originalName = file.getName().replace(".txt", "");
+                long startTime = System.currentTimeMillis();
+                compressUtil.lzmaDecompress(new File("./" + originalName + "Compressed.lzma"));
+                long endTime = System.currentTimeMillis();
+                System.out.println("Decompression time of: " + file + " is " + (endTime - startTime) + "ms");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        //compressUtil.lzmaCompress(loadedFilesForCompression.get(0));
+
     }
 }
